06830797722c114b619db5ed0b017491a8692254
Jack-Culcasi <giacomofculcasi@gmail.com>
2024-11-07 20:33:05 +0000

    def home added

--- home/views.py
+++ home/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import NetWorth
 
 @login_required
-def home(request):
-    return render(request, 'home.html')
+def home(request):    
+    if NetWorth.objects.filter(user=request.user).exists():
+        net_worth = NetWorth.objects.filter(user=request.user).latest('date')
+    else:
+        net_worth = 0  # Handle the case when no record exists
+    
+    # Prepare the context for template
+    context = {
+        'net_worth': net_worth
+    }
+    
+    return render(request, 'home.html', context)
+
+@login_required
+def payday(request):
+    context = {}
+    return render(request, 'payday.html', context)
