9f2a8a584ec14cb0367dad1a46d203b8aa287bb9
Jack-Culcasi <giacomofculcasi@gmail.com>
2024-11-09 13:44:44 +0000

    add_transaction view added, code updated on other views

--- home/views.py
+++ home/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
+from django.http import JsonResponse
 from datetime import datetime
-from .models import NetWorth, Payday
+from .models import NetWorth, Payday, Category, Transaction
 
 @login_required
 def home(request):    
@@ -25,12 +26,14 @@ def payday(request):
         # Get the form data
         amount = request.POST.get('amount')
         payday_date = request.POST.get('date')
+        note = request.POST.get('note', None)
         payday_date = timezone.make_aware(datetime.strptime(payday_date, '%Y-%m-%d'))        
         # Create the Payday object
         payday = Payday.objects.create(
             user=request.user,
             payday_date=payday_date,
-            amount=amount
+            amount=amount,
+            note=note
         )
         return redirect('expenses', payday_id=payday.id)
 
@@ -52,8 +55,45 @@ def expenses(request, payday_id=None):
         payday = Payday.objects.get(id=payday_id, user=request.user)
     except Payday.DoesNotExist:
         return redirect('payday')
+    
+    categories = Category.objects.all()
+    transactions = Transaction.objects.filter(user=request.user)
 
     context = {
         'payday': payday,
+        'categories': categories,
+        'transactions': transactions,
     }
     return render(request, 'expenses.html', context)
+
+@login_required
+def add_transaction(request):
+    if request.method == 'POST':
+        amount = request.POST.get('amount')
+        #category_id = request.POST.get('category', None)
+        note = request.POST.get('note', '')  # Default to an empty string if no note is provided
+
+        try:
+            #category = Category.objects.get(id=category_id)
+            transaction = Transaction.objects.create(
+                user=request.user,
+                amount=amount,
+                #category=category,
+                note=note
+            )
+
+            # Return success response with the new transaction data
+            return JsonResponse({
+                'success': True,
+                #'category_name': category.name,
+                'amount': amount
+            })
+
+        except Category.DoesNotExist:
+            return JsonResponse({'success': False, 'error': 'Category not found'})
+
+    return JsonResponse({'success': False, 'error': 'Invalid request'})
+
+@login_required
+def paydays(request):
+    return render(request, 'paydays.html')
