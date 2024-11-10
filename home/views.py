8891d2215d6b3a44cd42751be3bd02c434313df4
Jack-Culcasi <giacomofculcasi@gmail.com>
2024-11-10 18:44:45 +0000

    categories view added, user related info call adjusted

--- home/views.py
+++ home/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from django.http import JsonResponse
 from datetime import datetime, timedelta
-from .models import NetWorth, Payday, Category, Transaction, MonthlyExpenses
+from .models import NetWorth, Payday, Category, Transaction, MonthlyExpenses, FixedCosts, UserPreferences
 
 @login_required
 def home(request):    
@@ -16,6 +16,9 @@ def home(request):
     transactions = Transaction.objects.all()
     monthly_expenses = MonthlyExpenses.objects.all()
     paydays = Payday.objects.all()
+    for transaction in transactions:
+        print(transaction.category)
+
     context = {
         'net_worth': net_worth,
         'transactions': transactions,
@@ -30,7 +33,7 @@ def payday(request):
     today_date = timezone.now().date().strftime('%Y-%m-%d')
     # Check if is first payday ever, if it is, prompt start date field
     try:
-        last_two_paydays = Payday.objects.all().order_by('-payday_date')[:2]
+        last_two_paydays = Payday.objects.filter(user=request.user).order_by('-payday_date')[:2]
             
     except IndexError: # Not enough paydays to create MonthlyExpenses
         last_two_paydays = None
@@ -57,7 +60,7 @@ def payday(request):
             end_date=payday_date - timedelta(days=1)
             )
         else:
-            last_two_paydays = Payday.objects.all().order_by('-payday_date')[:2]
+            last_two_paydays = Payday.objects.filter(user=request.user).order_by('-payday_date')[:2]
             # Create the MonthlyExpenses object for the previous month, 
             # the start date is related to the payday date of the previous month, the end date is the last payday date minus one day.
             # If you are paid every 1st of the month you want your monthly expenses be calculated from the 1st to the 31st.
@@ -87,8 +90,9 @@ def expenses(request, payday_id=None, monthly_expense_id=None):
     except Payday.DoesNotExist:
         return redirect('payday')
     
+    fixed_costs = FixedCosts.objects.filter(user=request.user)
     monthly_expenses = MonthlyExpenses.objects.get(id=monthly_expense_id)
-    categories = Category.objects.all()
+    categories = Category.objects.filter(user=request.user)
     transactions = Transaction.objects.filter(user=request.user, monthly_expenses=monthly_expenses)
 
     context = {
@@ -96,6 +100,7 @@ def expenses(request, payday_id=None, monthly_expense_id=None):
         'categories': categories,
         'transactions': transactions,
         'monthly_expense_id': monthly_expense_id,
+        'fixed_costs': fixed_costs,
     }
     return render(request, 'expenses.html', context)
 
@@ -104,25 +109,30 @@ def add_transaction(request):
     if request.method == 'POST':
         amount = request.POST.get('amount')
         monthly_expense_id = request.POST.get('monthly_expense_id')
-        #category_id = request.POST.get('category', None)
-        note = request.POST.get('note', '')  # Default to an empty string if no note is provided
+        category_id = request.POST.getlist('categories')
+        note = request.POST.get('note', '')
+
         monthly_expenses = MonthlyExpenses.objects.get(id=monthly_expense_id)
+        if category_id:
+
+            category = Category.objects.get(id=int(category_id[0]))
+        else:
+            category = None
 
         try:
-            #category = Category.objects.get(id=category_id)
             transaction = Transaction.objects.create(
                 user=request.user,
                 monthly_expenses=monthly_expenses,
                 amount=amount,
-                #category=category,
+                category=category,
                 note=note
             )
 
             # Return success response with the new transaction data
             return JsonResponse({
                 'success': True,
-                #'category_name': category.name,
-                'amount': amount
+                'amount': amount,
+                'symbol': request.user.userpreferences.currency_symbol,
             })
 
         except Category.DoesNotExist:
@@ -132,7 +142,7 @@ def add_transaction(request):
 
 @login_required
 def paydays(request):
-    paydays = Payday.objects.all()
+    paydays = Payday.objects.filter(user=request.user)
     if request.method == 'POST':
         # The Delete button is pressed
         payday_id = request.POST.get('payday_id')
@@ -155,3 +165,21 @@ def monthly_expenses(request, payday_id):
     }
 
     return render(request, 'monthly_expenses.html', context)
+
+@login_required
+def categories(request):
+    categories = Category.objects.filter(user=request.user)
+
+    if request.method == 'POST':
+        category_name = request.POST.get('name')
+        category_note = request.POST.get('note', None)
+        new_category = Category.objects.create(
+            user=request.user,
+            name=category_name,
+            note=category_note
+        )
+
+    context = {
+        'categories': categories
+    }
+    return render(request, 'categories.html', context)
