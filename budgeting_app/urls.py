5ddaac941d24ac07ebd312e32e559dcaf3411d6d
Jack-Culcasi <giacomofculcasi@gmail.com>
2024-11-13 20:40:48 +0000

    first time user path added

--- budgeting_app/urls.py
+++ budgeting_app/urls.py
@@ -36,6 +36,7 @@ urlpatterns = [
     path('delete_payday/', views.delete_payday, name='delete_payday'),
     path('payday_fixed_costs/<int:payday_id>/<int:monthly_expense_id>/', views.payday_fixed_costs, name='payday_fixed_costs'),
     path('fixed_costs/', views.fixed_costs, name='fixed_costs'),
-    path('deductions/<int:payday_id>/<int:monthly_expense_id>/', views.deductions, name='deductions')
+    path('deductions/<int:payday_id>/<int:monthly_expense_id>/', views.deductions, name='deductions'),
+    path('first_time_user/', views.first_time_user, name='first_time_user'),
 
 ]
