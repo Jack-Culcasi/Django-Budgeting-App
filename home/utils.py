647d6d38f965ab7e955ef5aec809f13e01ae507d
Jack-Culcasi <giacomofculcasi@gmail.com>
2024-11-17 14:24:49 +0000

    update pensions, investments and savings functions added

--- home/utils.py
+++ home/utils.py
@@ -31,3 +31,68 @@ def get_deductions(full_amounts, split_fixed_costs, deduction_amount):
     for amount in split_fixed_costs:
         deductions += float(amount) / 2
     return Decimal(deductions)
+
+def update_investments(request, new_amounts, investment_ids, broker_ids):
+    try:
+        # Create a list of lists containing [investment_id, new_amount, broker_id]
+        investments_data = [] # [ [1, 200, 1], [2, 100, 2] ]
+        for x in range(len(new_amounts)):
+            investments_data.append([
+                int(investment_ids[x]),
+                Decimal(new_amounts[x]),
+                int(broker_ids[x]),
+            ])
+
+        # Update bank record for every id
+        for element in investments_data:
+            broker = Broker.objects.get(user=request.user, id=element[2])
+            investment = Investment.objects.get(broker=broker, id=element[0])
+            investment.amount = element[1] 
+            investment.save()
+
+        return True    
+    
+    except Exception as e:
+        # Log the exception if needed
+        print(f"Error updating investments: {e}")
+        return False  
+    
+def update_pensions(request, new_amounts, pension_ids):
+    try:
+        # Create a list of tuples containg (id, amount)
+        amount_ids_list = [] # [(1, 500), (2,300)]
+        for x in range(0, len(new_amounts)):
+            amount_ids_list.append((int(pension_ids[x]), Decimal(new_amounts[x])))
+        
+        # Update pension record
+        for element in amount_ids_list:
+            pension = Pension.objects.get(user=request.user, id=element[0])
+            pension.amount = element[1] 
+            pension.save()
+
+        return True    
+    
+    except Exception as e:
+        # Log the exception if needed
+        print(f"Error updating pension: {e}")
+        return False  
+    
+def update_savings(request, new_amounts, bank_ids):
+    try:
+        # Create a list of tuples containg (id, amount)
+        amount_ids_list = [] # [(1, 500), (2,300)]
+        for x in range(0, len(new_amounts)):
+            amount_ids_list.append((int(bank_ids[x]), Decimal(new_amounts[x])))
+
+        # Update pension record
+        for element in amount_ids_list:
+            bank = Bank.objects.get(user=request.user, id=element[0])
+            bank.amount = element[1] 
+            bank.save()
+
+        return True    
+    
+    except Exception as e:
+        # Log the exception if needed
+        print(f"Error updating pension: {e}")
+        return False  
