f89bbe6160367fa83c40c74812dc7defdbc7a4da
Jack-Culcasi <giacomofculcasi@gmail.com>
2024-12-09 21:20:09 +0000

    create_groceries_category function added

--- home/signals.py
+++ home/signals.py
@@ -1,20 +1,18 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import MonthlyExpenses, Category
+from django.contrib.auth.models import User
+from .models import Category, UserPreferences
 
-@receiver(post_save, sender=MonthlyExpenses)
-def create_categories_for_first_expense(sender, instance, created, **kwargs):
-    if created:  # Only trigger on creation of a new MonthlyExpenses object
-        user = instance.payday.user  # Get the user associated with the MonthlyExpenses object
-        
-        # Check if any MonthlyExpenses exist for this user
-        if not MonthlyExpenses.objects.filter(payday__user=user).exists():  # If no other MonthlyExpenses exist
-            
-            # Create the default categories
-            categories = [
-                {'name': 'Grocery', 'note': "I have to eat after all"},
-                {'name': 'Miscellaneous', 'note': "I have bought a rally car for myself!"}
-            ]
-            
-            for category in categories:
-                Category.objects.create(user=user, name=category['name'], note=category['note'])
+@receiver(post_save, sender=User)
+def create_groceries_category(sender, instance, created, **kwargs):
+    if created:  # Only create the category if the user is new
+        # Create the Groceries category for the new user
+        Category.objects.create(
+            user=instance,  
+            name='Groceries',
+            note='I have to eat after all' 
+        )
+        # Create UserPreferences instance
+        UserPreferences.objects.create(
+            user=instance,
+        )
