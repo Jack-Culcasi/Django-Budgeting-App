bc96d83b6aae3ebd5e993b4ab18f36bb710f4a4d
Jack-Culcasi <giacomofculcasi@gmail.com>
2024-12-09 21:19:05 +0000

    custom user creation class added

--- home/forms.py
+++ home/forms.py
@@ -1,4 +1,44 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class CustomUserCreationForm(UserCreationForm):
+    password1 = forms.CharField(
+        label="Password",
+        strip=False,
+        widget=forms.PasswordInput(attrs={
+            'class': 'input input-bordered w-full',
+            'placeholder': 'Password',
+        }),
+        help_text="Choose a strong password."
+    )
+
+    password2 = forms.CharField(
+        label="Confirm Password",
+        strip=False,
+        widget=forms.PasswordInput(attrs={
+            'class': 'input input-bordered w-full',
+            'placeholder': 'Confirm Password',
+        }),
+        help_text="Re-enter your password for verification."
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
+        labels = {
+            'username': 'Username',
+        }
+        help_texts = {
+            'username': None,  # Removes the verbose default username help text
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({
+            'class': 'input input-bordered w-full',
+            'placeholder': 'Username',
+        })
 
 class UploadFileForm(forms.Form):
     file = forms.FileField()
